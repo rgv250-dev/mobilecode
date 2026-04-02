7e8bc390c87398c18d6a9b8d074596007eb4581b
rgv250 <asr1203@gmail.com>
2026-04-02 13:53:46 +0900

    서버별 타임 보는 앱 비슷한거 제작
    서버별 타임 보는 앱 비슷한거 제작 딱히 대단한건 없음

---
+++ AOS/servertimer/app/src/main/java/com/example/servertimer/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.servertimer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+// 시계 쪽 배경 컬러
+val NeonBackground = Color(0xFF050816)
+val NeonPanelBg = Color(0xFF070b19)
+val NeonAccentCyan = Color(0xFF00F5FF)
+val NeonAccentPink = Color(0xFFFF4ECD)
+val NeonTextPrimary = Color(0xFFEAFFFD)
+val NeonDividerColor = Color(0x66FFFFFF)
